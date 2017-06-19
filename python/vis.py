9ac3310e8d36b9ad714d98d005ddf40a2a6d075d
dgole <dgole100@gmail.com>
2017-06-19 16:21:16 -0600

    Update vis.py

--- python/vis.py
+++ python/vis.py
@@ -10,6 +10,14 @@ import microphone
 import led
 import tools
 
+def create_mel_bank():
+    global samples, mel_y, mel_x
+    samples = int(config.MIC_RATE * config.N_ROLLING_HISTORY / (2.0 * config.FPS))
+    mel_y, (_, mel_x) = compute_melmat(num_mel_bands=config.N_FFT_BINS,
+                                             freq_min=config.MIN_FREQUENCY,
+                                               freq_max=config.MAX_FREQUENCY,
+                                               num_fft_bands=samples,
+                                               sample_rate=config.MIC_RATE)
 
 samples = None
 mel_y = None
