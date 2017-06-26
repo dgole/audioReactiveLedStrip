1a54d02a99193cdebf1fa0253872d14b10994ba7
dgole <dgole100@gmail.com>
2017-06-25 21:52:10 -0600

    Update tools.py

--- python/tools.py
+++ python/tools.py
@@ -155,7 +155,7 @@ class Runner:
             if i == self.locInt:
                 self.outArray[i] = 1.0
             else:
-                self.outArray[i] = 1.0 - np.abs(i-self.locInt)/n + 0.2
+                self.outArray[i] = 1.0
     def update(self):
         self.locFloat = self.locFloat + self.speed
         if int(self.locFloat) != self.locInt:
