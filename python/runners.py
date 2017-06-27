92f50bd43e202324b885636744f6c0a1f79aa1ce
dgole <dgole100@gmail.com>
2017-06-26 20:59:40 -0600

    Update runners.py

--- python/runners.py
+++ python/runners.py
@@ -75,7 +75,7 @@ volume = tools.ExpFilter(config.MIN_VOLUME_THRESHOLD, alpha_decay=0.02, alpha_ri
 runObjList = []
 colorChoices = ['r','g','b','p']
 for i in range(5):
-    runObjList.append(tools.Runner(np.random.randint(1,5), 
+    runObjList.append(tools.Runner(np.random.randint(3,7), 
                                    np.random.random()/2.0 + 0.05, 
                                    colorChoices[np.random.randint(0,4)], 
                                    np.random.randint(0,config.N_PIXELS-1)
