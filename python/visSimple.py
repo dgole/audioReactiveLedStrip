f6da125a44717ed3e76400545913df48df9f904b
dgole <dgole100@gmail.com>
2017-08-04 18:43:35 -0600

    Update visSimple.py

--- python/visSimple.py
+++ python/visSimple.py
@@ -77,7 +77,7 @@ chordObj = tools.Chord(0.02)
 beatObj = tools.Beat(0.5)
 
 rawFilt = tools.ExpFilter(np.tile(0.01, config.N_PIXELS), alpha_decay=0.99, alpha_rise=0.99)
-ledFilt = tools.ExpFilter(np.tile(0.01, config.N_PIXELS), alpha_decay=0.5, alpha_rise=0.9)
+ledFilt = tools.ExpFilter(np.tile(0.01, config.N_PIXELS), alpha_decay=0.3, alpha_rise=0.99)
 _prev_spectrum = np.tile(0.01, config.N_PIXELS)
 mel_gain = tools.ExpFilter(np.tile(1e-1, config.N_FFT_BINS), alpha_decay=0.1, alpha_rise=0.99)
 volume = tools.ExpFilter(config.MIN_VOLUME_THRESHOLD, alpha_decay=0.02, alpha_rise=0.02)
@@ -103,8 +103,8 @@ def visualize_spectrum(y):
     r = np.zeros_like(temp1) + 0.2
     g = np.zeros_like(temp1)
     b = np.zeros_like(temp1)
-    r[0:np.min([int(bassPower),config.N_PIXELS])]+=0.5
-    r += np.max(bassPower/config.N_PIXELS, 0.3)
+    b[0:np.min([int(bassPower),config.N_PIXELS])]+=1.0
+    r += np.max(bassPower/config.N_PIXELS, 0.8)
     output = np.array([r,g,b]) * 255
     output2 = np.zeros([3, 2*config.N_PIXELS])
     output2[..., 0:config.N_PIXELS] = output[...,::-1]
