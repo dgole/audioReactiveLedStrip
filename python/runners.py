8327ba19f2411ba99e9f4c0118f78f79c4f040b3
dgole <dgole100@gmail.com>
2017-08-05 18:27:39 -0600

    Update runners.py

--- python/runners.py
+++ python/runners.py
@@ -5,7 +5,7 @@ import sys
 import numpy as np
 from numpy import *
 from scipy.ndimage.filters import gaussian_filter1d
-import config
+import configRunners as config
 import microphone
 import led
 import tools
