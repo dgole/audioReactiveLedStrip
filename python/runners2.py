c2feb4670747c08408e5a2f7bf29bf4bc2fa2058
dgole <dgole100@gmail.com>
2017-08-07 21:40:03 -0600

    Update runners2.py

--- python/runners2.py
+++ python/runners2.py
@@ -90,7 +90,7 @@ def visualize_spectrum(y):
     #y = np.copy(interpolate(y, config.N_PIXELS))
     #_prev_spectrum = np.copy(y)
     temp2 = ledFilt.update(y)
-    bassPower = np.sum(temp2[0:5])*10
+    bassPower = np.sum(temp2[0:5])
     print(int(bassPower))
     # Color channel mappings
     for j in range(len(runObjList)):
