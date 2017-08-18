13d2dedc4b5a9b674cb11b99281beebe595cd8ca
dgole <dgole100@gmail.com>
2017-08-17 20:48:36 -0600

    Update runners2.py

--- python/runners2.py
+++ python/runners2.py
@@ -100,9 +100,10 @@ def visualize_spectrum(y):
     #output = np.array([r,g,b]) * 255
     #output = np.array([np.flipud(r),np.flipud(g),np.flipud(b)]) * 255
     #output2 = np.array([output,output]).flatten()
-    output2 = np.zeros([3, config.N_PIXELS])
-    output2[..., 0:local_N_PIXELS] = output
-    output2[..., local_N_PIXELS:2*local_N_PIXELS] = output[...,::-1]
+    output2 = np.tile(output, config.calcFactor)
+    #output2 = np.zeros([3, config.N_PIXELS])
+    #output2[..., 0:local_N_PIXELS] = output
+    #output2[..., local_N_PIXELS:2*local_N_PIXELS] = output[...,::-1]
     return output2
     
 
