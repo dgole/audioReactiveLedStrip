caeea3293a9e99fd1ef839861b4e99e167d44658
dgole <dgole100@gmail.com>
2017-08-17 20:39:51 -0600

    Update config.py

--- python/config.py
+++ python/config.py
@@ -39,7 +39,7 @@ MIN_FREQUENCY = 130.81 * 1.0
 MAX_FREQUENCY = 9397.27 * 0.1
 """Frequencies above this value will be removed during audio processing"""
 
-N_FFT_BINS = N_PIXELS // 5
+N_FFT_BINS = 60
 """Number of frequency bins to use when transforming audio to frequency domain
 
 Fast Fourier transforms are used to transform time-domain audio data to the
