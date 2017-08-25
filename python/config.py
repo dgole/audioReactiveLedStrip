361e38bf93798a58b6ea2469ff731ffacb97decb
dgole <dgole100@gmail.com>
2017-08-24 21:41:15 -0600

    Update config.py

--- python/config.py
+++ python/config.py
@@ -36,8 +36,8 @@ MIN_FREQUENCY = 130.81 * 1.0
 
 # 4066.84 is b7 and a half
 # 3951.066 is b7
-MAX_FREQUENCY = 3951.066 * 1.0
-#MAX_FREQUENCY = 9397.27 * 0.1
+#MAX_FREQUENCY = 3951.066 * 1.0
+MAX_FREQUENCY = 9397.27 * 0.1
 """Frequencies above this value will be removed during audio processing"""
 
 N_FFT_BINS = 75
