2be6f584b3a47812f2ae218c16308269e138885d
dgole <dgole100@gmail.com>
2017-08-24 20:08:59 -0600

    Update beat1.py

--- python/beat1.py
+++ python/beat1.py
@@ -10,6 +10,8 @@ import microphone
 import led
 import tools
 
+local_N_PIXELS = config.N_PIXELS // config.calcFactor
+
 def create_mel_bank():
     global samples, mel_y, mel_x
     samples = int(config.MIC_RATE * config.N_ROLLING_HISTORY / (2.0 * config.FPS))
@@ -76,9 +78,9 @@ keyObj = tools.Key(determineKeyMatrix, 0.001)
 chordObj = tools.Chord(0.02)
 beatObj = tools.Beat(0.5)
 
-rawFilt = tools.ExpFilter(np.tile(0.01, config.N_PIXELS), alpha_decay=0.99, alpha_rise=0.99)
-ledFilt = tools.ExpFilter(np.tile(0.01, config.N_PIXELS), alpha_decay=0.2, alpha_rise=0.7)
-_prev_spectrum = np.tile(0.01, config.N_PIXELS)
+rawFilt = tools.ExpFilter(np.tile(0.01, local_N_PIXELS), alpha_decay=0.99, alpha_rise=0.99)
+ledFilt = tools.ExpFilter(np.tile(0.01, local_N_PIXELS), alpha_decay=0.2, alpha_rise=0.7)
+_prev_spectrum = np.tile(0.01, local_N_PIXELS)
 mel_gain = tools.ExpFilter(np.tile(1e-1, config.N_FFT_BINS), alpha_decay=0.05, alpha_rise=0.99)
 volume = tools.ExpFilter(config.MIN_VOLUME_THRESHOLD, alpha_decay=0.02, alpha_rise=0.02)
 
@@ -91,7 +93,7 @@ a2 = [0,0,0]
 def visualize_spectrum(y):
     """Effect that maps the Mel filterbank frequencies onto the LED strip"""
     global _prev_spectrum, colorThisTime, count0, nFramesCycle, a1, a2
-    #y = np.copy(interpolate(y, config.N_PIXELS))
+    #y = np.copy(interpolate(y, local_N_PIXELS))
     _prev_spectrum = np.copy(y)
     # Color channel mappings
     count0+=1
@@ -105,7 +107,7 @@ def visualize_spectrum(y):
     
     bassPower = np.sum(temp2[0:5])*10
     print(int(bassPower))
-    iMax = np.min([int((bassPower*10)**0.5),config.N_PIXELS])
+    iMax = np.min([int((bassPower*10)**0.5),local_N_PIXELS])
     countEff = count0%nFramesCycle
     
     r = np.zeros_like(temp1)
@@ -136,18 +138,17 @@ def visualize_spectrum(y):
         
     
     
-    r+=a1[0]*0.1; r+=a1[0]*np.max(bassPower/config.N_PIXELS, 0.9)
-    g+=a1[1]*0.1; g+=a1[1]*np.max(bassPower/config.N_PIXELS, 0.9)
-    b+=a1[2]*0.1; b+=a1[2]*np.max(bassPower/config.N_PIXELS, 0.9)
+    r+=a1[0]*0.1; r+=a1[0]*np.max(bassPower/local_N_PIXELS, 0.9)
+    g+=a1[1]*0.1; g+=a1[1]*np.max(bassPower/local_N_PIXELS, 0.9)
+    b+=a1[2]*0.1; b+=a1[2]*np.max(bassPower/local_N_PIXELS, 0.9)
     
     r[0:iMax]+=a2[0]*1.0
     g[0:iMax]+=a2[1]*1.0
     b[0:iMax]+=a2[2]*1.0
    
     output = np.array([r,g,b]) * 255
-    output2 = np.zeros([3, 2*config.N_PIXELS])
-    output2[..., 0:config.N_PIXELS] = output[...,::-1]
-    output2[..., config.N_PIXELS:2*config.N_PIXELS] = output
+    output2[..., 0:local_N_PIXELS] = output
+    output2[..., local_N_PIXELS:2*local_N_PIXELS] = output[...,::-1]
     return output2
 
 fft_window = np.hamming(int(config.MIC_RATE / config.FPS) * config.N_ROLLING_HISTORY)
@@ -164,7 +165,7 @@ def microphone_update(audio_samples):
     vol = np.max(np.abs(y_data))
     if vol < config.MIN_VOLUME_THRESHOLD:
         print('No audio input. Volume below threshold. Volume:', vol)
-        led.pixels = np.tile(0, (3, 2*config.N_PIXELS))
+        led.pixels = np.tile(0, (3, config.N_PIXELS))
         led.update()
     else:
         # Transform audio input into the frequency domain
