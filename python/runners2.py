d706509d4e7def30ae7382fdf9392d4276a93ca1
dgole <dgole100@gmail.com>
2017-08-24 20:01:45 -0600

    Update runners2.py

--- python/runners2.py
+++ python/runners2.py
@@ -79,7 +79,7 @@ colorChoices = ['r','g','b','p']
 startColorIndex = np.random.randint(0,4)
 for i in range(int(sys.argv[1])):
     runObjList.append(tools.Runner(np.random.randint(int(sys.argv[2]),int(sys.argv[3])), 
-                                   np.random.random()/2.0 + 0.05, 
+                                   (np.random.random() + 0.1)*float(sys.argv[4]), 
                                    colorChoices[(startColorIndex+i)%4], 
                                    np.random.randint(10,local_N_PIXELS-10)
                                   ))
